ac12ebafb38bc7a9299f3aa92861728a986f3f34
Kaycee Nuel <heykayceee@gmail.com>
2024-09-19 08:39:15 +0100

    updated main.go

--- main.go
+++ main.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -14,30 +12,12 @@ func main() {
 	http.HandleFunc("/404", status404Handler)
 	http.HandleFunc("/500", status500Handler)
 	http.HandleFunc("/authenticated", authHandler)
+	http.HandleFunc("/limited", limitHandler)
 
 	fmt.Println("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the home page!")
-}
-
-func status200Handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "200 OK")
-}
-
-func status404Handler(w http.ResponseWriter, r *http.Request) {
-	http.NotFound(w, r)
-}
-
-func status500Handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusInternalServerError)
-	fmt.Fprintf(w, "500 Internal Server Error")
 }
