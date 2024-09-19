e04c5ca01f5794909c6ac71fd2d0e06d58124eb3
Kaycee Nuel <heykayceee@gmail.com>
2024-09-19 08:37:12 +0100

    refactored authHanlder, updated chechCredentials

--- auth.go
+++ auth.go
@@ -9,12 +9,13 @@ import (
 func authHandler(w http.ResponseWriter, r *http.Request) {
 	username, password, ok := r.BasicAuth()
 	if !ok || !checkCredentials(username, password) {
-		w.Header().Set("WWW-Authenticate", `http-StatusUnauthorized`)
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
-	fmt.Fprintf(w, "Hello, %s!", username)
+	fmt.Fprintf(w, "Welcome, %s!", username)
 }
 
 func checkCredentials(username, password string) bool {
-	return username == os.Getenv("AUTH_USERNAME") && password == os.Getenv("Auth_PASSWORD")
+	return username == os.Getenv("AUTH_USERNAME") && password == os.Getenv("AUTH_PASSWORD")
 }
